c67768006c4b9dd51a4ee6155996ea82580f5a5b
monmun018 <quymao08101999@gmail.com>
2024-06-05 12:20:14 +0900

    mao

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,11 @@
 <template>
-    <div>
-        
-    </div>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class XXXComponent extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
